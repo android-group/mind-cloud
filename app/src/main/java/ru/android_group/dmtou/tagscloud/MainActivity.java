61f125e4689aeaddd1bc2ccc4924590d33a7e474
ilarionovdm <ilarionovdm@gmail.com>
2016-05-18 01:32:59 +0300

    Add opportunity to create TAGS

--- app/src/main/java/ru/android_group/dmtou/tagscloud/MainActivity.java
+++ app/src/main/java/ru/android_group/dmtou/tagscloud/MainActivity.java
@@ -2,24 +2,36 @@ package ru.android_group.dmtou.tagscloud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private static final int[] ID = { 0,1,2,3,4,5,6,7,8,9 };
-    private Button[] mainSpisok = new Button[10];
+    private static final int[] ID = { 0,1,2,3,4,5,6,7,8,9 };  //ID тегов
+    private Button[] mainSpisok = new Button[10];              //список тегов (заменить на label)
+    private int k = 1;                                       //начальный номер ID тегов (0 под кнопку добавить)
+    private static final int[] IDLAYOUTS = {10001,10002};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        LinearLayout layout = new LinearLayout(this);
-        layout.setOrientation(LinearLayout.VERTICAL);
-        layout.setLayoutParams(new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        for (int i = 0;i<10; i++) {
+        //                    //MAIN LAYOUT
+        LinearLayout layoutMain = new LinearLayout(this);
+        LinearLayout.LayoutParams lllp0 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,LinearLayout.LayoutParams.FILL_PARENT);
+        layoutMain.setOrientation(LinearLayout.VERTICAL);
+        layoutMain.setLayoutParams(lllp0);
+        this.addContentView(layoutMain,lllp0);
+        //                    //LAYOUT с кнопкой добавить
+        LinearLayout layoutAdd = new LinearLayout(this);
+        layoutAdd.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams lllp1 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        lllp1.weight = 10;
+        layoutAdd.setLayoutParams(lllp1);
+        layoutAdd.setId(IDLAYOUTS[0]);
+        /*for (int i = 0;i<10; i++) {
             mainSpisok[i] = new Button(this);
             mainSpisok[i].setId(ID[i]);
             mainSpisok[i].setText("Элемент " +String.valueOf(ID[i])+" главного списка");
@@ -28,9 +40,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mainSpisok[i].setOnClickListener(this);
             //mainSpisok[i].setVisibility(View.VISIBLE);
             layout.addView(mainSpisok[i]);
-        }
-        this.addContentView(layout, new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        }*/
+        //                    //Кнопка ДОБАВИТЬ
+        Button addTag = new Button(this);
+        addTag.setId(ID[0]);
+        addTag.setText("Добавить тег");
+        LinearLayout.LayoutParams lllp2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        lllp2.gravity = Gravity.RIGHT;
+        addTag.setLayoutParams(lllp2);
+        addTag.setOnClickListener(this);
+        layoutAdd.addView(addTag,lllp2);
+        layoutMain.addView(layoutAdd,lllp1);
+        //                    //LAYOUT с тегами
+        LinearLayout layoutTags = new LinearLayout(this);
+        layoutTags.setOrientation(LinearLayout.VERTICAL);
+        LinearLayout.LayoutParams lllp3 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        lllp3.weight = 1;
+        layoutTags.setId(IDLAYOUTS[1]);
+        /*Button b1 = new Button(this);
+        b1.setLayoutParams(lllp2);
+        layoutTags.addView(b1,lllp2);*/
+        layoutMain.addView(layoutTags,lllp3);
     }
 
     @Override
@@ -38,9 +68,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         //mainSpisok[0].setText(v.getClass().getName());
         //if (v.getClass().getName().equals("android.widget.Button")) mainSpisok[1].setText("da");
         //mainSpisok[2].setText(String.valueOf(mainSpisok[2].getId()));
-        switch (v.getClass().getName()) {
+        /*switch (v.getClass().getName()) {
             case "android.widget.Button":
                 mainSpisok[v.getId()].setText( "Нажата кнопка с индексом " + String.valueOf(v.getId()) );
+            break;
+        }*/
+        //                  //Обработчик нажатия на кнопку добавить (максимум 10 тегов)
+        switch (v.getId()) {
+            case 0:{
+                if (k<10) {
+                    LinearLayout layoutTags = (LinearLayout) findViewById(IDLAYOUTS[1]);
+                    mainSpisok[k] = new Button(this);
+                    mainSpisok[k].setId(ID[k]);
+                    mainSpisok[k].setText("Элемент " + String.valueOf(ID[k]) + " главного списка");
+                    mainSpisok[k].setLayoutParams(new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    mainSpisok[k].setOnClickListener(this);
+                    layoutTags.addView(mainSpisok[k]);
+                    k++;
+                }
+            }
             break;
         }
     }
